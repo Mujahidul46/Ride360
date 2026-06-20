a14a2784e86f82a6b3261f8f1aca593de136404c
Mujahidul Islam <mujahidul46@outlook.com>
2026-06-20 22:40:11 +0100

    Added social and reminders nav components

--- ride-360-ui/src/app/app.routes.ts
+++ ride-360-ui/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { Rides } from './components/rides/rides';
 import { AddExpense } from './components/add-expense/add-expense';
 import { ToggleRide } from './components/toggle-ride/toggle-ride';
 import { AuthGuard } from './services/auth-guard.service';
+import { Social } from './components/social/social';
+import { Reminders } from './components/reminders/reminders';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // empty path makes it so that login page is first one to load when app starts
@@ -13,6 +15,8 @@ export const routes: Routes = [
     { path: 'signup', component: SignupComponent },
     { path: 'add-expense', component: AddExpense, canActivate: [AuthGuard] },
     { path: 'rides', component: Rides, canActivate: [AuthGuard] },
+    { path: 'social', component: Social, canActivate: [AuthGuard] },
+    { path: 'reminders', component: Reminders, canActivate: [AuthGuard]},
     { path: 'stats', component: Stats, canActivate: [AuthGuard] },
     { path: 'start-stop-ride', component: ToggleRide, canActivate: [AuthGuard] },
 ];
